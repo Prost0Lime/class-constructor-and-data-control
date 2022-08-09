c5905e6f8f274bb71670e50e01cf3fb04e7ec34a
Prost0Lime <s.shishkov2026@gmail.com>
2022-08-09 13:59:38 +0300

    class constructor and data control

---
+++ laba3/MainForm.Designer.cs
@@ -0,0 +1,316 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: adfgh
+ * Date: 15-03-2019
+ * Time: 14:38
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace laba3
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton4;
+		private System.Windows.Forms.RadioButton radioButton5;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox8;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.radioButton4 = new System.Windows.Forms.RadioButton();
+			this.radioButton5 = new System.Windows.Forms.RadioButton();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(138, 25);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(138, 71);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(20, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "высота";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(20, 68);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "ширина ";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(329, 280);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "ввод";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(329, 311);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "вывод";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.Location = new System.Drawing.Point(329, 22);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(104, 24);
+			this.radioButton1.TabIndex = 7;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "объект1";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1CheckedChanged);
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.Location = new System.Drawing.Point(329, 62);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(104, 24);
+			this.radioButton2.TabIndex = 9;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "объект2";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2CheckedChanged);
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.Location = new System.Drawing.Point(329, 112);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(104, 24);
+			this.radioButton3.TabIndex = 11;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "объект3";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3CheckedChanged);
+			// 
+			// radioButton4
+			// 
+			this.radioButton4.Location = new System.Drawing.Point(329, 157);
+			this.radioButton4.Name = "radioButton4";
+			this.radioButton4.Size = new System.Drawing.Size(104, 24);
+			this.radioButton4.TabIndex = 12;
+			this.radioButton4.TabStop = true;
+			this.radioButton4.Text = "объект4";
+			this.radioButton4.UseVisualStyleBackColor = true;
+			this.radioButton4.CheckedChanged += new System.EventHandler(this.RadioButton4CheckedChanged);
+			// 
+			// radioButton5
+			// 
+			this.radioButton5.Location = new System.Drawing.Point(329, 198);
+			this.radioButton5.Name = "radioButton5";
+			this.radioButton5.Size = new System.Drawing.Size(104, 24);
+			this.radioButton5.TabIndex = 13;
+			this.radioButton5.TabStop = true;
+			this.radioButton5.Text = "объект5";
+			this.radioButton5.UseVisualStyleBackColor = true;
+			this.radioButton5.CheckedChanged += new System.EventHandler(this.RadioButton5CheckedChanged);
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(20, 113);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 14;
+			this.label3.Text = "длина";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(138, 119);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 15;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(138, 166);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 17;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(138, 201);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 20);
+			this.textBox6.TabIndex = 18;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(138, 239);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(100, 20);
+			this.textBox7.TabIndex = 19;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(20, 162);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 21;
+			this.label4.Text = "высота";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(20, 197);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 23);
+			this.label5.TabIndex = 22;
+			this.label5.Text = "ширина";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(20, 239);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(100, 23);
+			this.label6.TabIndex = 24;
+			this.label6.Text = "длина";
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(20, 282);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(100, 23);
+			this.label7.TabIndex = 26;
+			this.label7.Text = "площадь";
+			// 
+			// label8
+			// 
+			this.label8.Location = new System.Drawing.Point(20, 316);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(100, 23);
+			this.label8.TabIndex = 27;
+			this.label8.Text = "объём";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(138, 282);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 28;
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(138, 313);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(100, 20);
+			this.textBox8.TabIndex = 29;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(449, 346);
+			this.Controls.Add(this.textBox8);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.radioButton5);
+			this.Controls.Add(this.radioButton4);
+			this.Controls.Add(this.radioButton3);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.Text = "длина";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+	}
+}
---
+++ laba3/MainForm.cs
@@ -0,0 +1,138 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: adfgh
+ * Date: 15-03-2019
+ * Time: 14:38
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace laba3
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		Parall P1 = new Parall(); 
+		Parall P2 = new Parall(7,17,10);
+		Parall P3 = new Parall(); 		
+		Parall P4 = new Parall();
+		Parall P5 = new Parall();
+		Parall P;
+	void Button1Click(object sender, EventArgs e)
+	{try {
+			if (!radioButton1.Checked && !radioButton2.Checked && !radioButton3.Checked && !radioButton4.Checked && !radioButton5.Checked) {
+		MessageBox.Show("Объект не выбран");}
+			else{
+	P.setA(Convert.ToDouble(textBox1.Text));
+	P.setB(Convert.ToDouble(textBox2.Text)); 
+	P.setC(Convert.ToDouble(textBox3.Text));
+				} 
+			
+		}catch(FormatException){
+				MessageBox.Show("Вы ввели не число!");
+			}
+		}
+	void Button2Click(object sender, EventArgs e) 
+{ 
+	try{
+	textBox5.Text=Convert.ToString(P.geta());
+	textBox6.Text=Convert.ToString(P.getb());
+	textBox7.Text=Convert.ToString(P.getc());
+	
+	textBox4.Text=Convert.ToString(P.getPloschad());
+	textBox8.Text=Convert.ToString(P.getObem());
+	}catch(NullReferenceException){
+		MessageBox.Show("вы не выбрали объект!");
+		}
+	}
+		void RadioButton1CheckedChanged(object sender, EventArgs e)
+		{
+			P = P1;
+		}
+		void RadioButton2CheckedChanged(object sender, EventArgs e)
+		{
+			P = P2;
+		}
+		void RadioButton3CheckedChanged(object sender, EventArgs e)
+		{
+			P = P3;
+		}
+		void RadioButton4CheckedChanged(object sender, EventArgs e)
+		{
+			P = P4;
+		}
+		void RadioButton5CheckedChanged(object sender, EventArgs e)
+		{
+			P = P5;
+		}
+		}
+	
+	
+class Parall { 
+
+private double a;
+private double b;
+private double c;
+	
+	public Parall()
+	{
+		a = 4;
+		b = 5	;
+		c = 3;
+	}
+	public Parall (double a, double b, double c){
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	public double geta () {return a;}
+	public double getb () {return b;} 
+	public double getc() {return c;} 
+	
+public void setA (double a1) {
+		if (a1<17 || a1>7) {
+			a=a1;
+	}else{
+		MessageBox.Show("Выход за диапазон");
+	}}
+public void setB (double b1) { 
+	if (b1>0)
+		b=b1;
+	else
+		MessageBox.Show("Отрицательные значения");	
+}
+public void setC (double c1) { 
+		if (c1>0){
+		c=c1;
+		}else{
+		MessageBox.Show("Отрицательные значения");
+}
+	}
+
+public double getPloschad () {
+	return 2*getc()*(geta()+getb());
+			}
+public double getObem () {
+	return geta()*getb()*getc();
+			}
+		}
+	}
+
